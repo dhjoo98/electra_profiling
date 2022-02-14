bc1202733d038b8948a6951f21a491c308b894b8
donghyeon <dhjoo98@korea.ac.kr>
2022-02-14 12:56:39 +0900

    profiling electra base&large memory performance on tensorboard

--- huggingface/electra_small_discriminator_batch.py
+++ huggingface/electra_small_discriminator_batch.py
@@ -1,42 +1,38 @@
+
 #그리 정확한 구현은 아닌 것 같다.
 from transformers import ElectraForPreTraining, ElectraTokenizerFast
 import torch
 from torch import nn
-#from torch.utils.tensorboard import SummaryWriter
-#from pytictoc import TicToc
+from torch.profiler import profile, record_function, ProfilerActivity #a new API from pytorch v1.8, used to be torch.autograd.profiler
 
-#for non-tensorboard profiler
-import numpy as np
-#import torch.autograd.profiler as profiler
 
-#writer = SummaryWriter() # Writer will output to ./runs/ directory by default
 
 class electra(nn.Module):
     def __init__(self):
         super(electra, self).__init__()
-        self.tokenizer = ElectraTokenizerFast.from_pretrained("google/electra-small-discriminator") #identical to Bert tokenizer fake_sentence
-        self.discriminator = ElectraForPreTraining.from_pretrained("google/electra-small-discriminator")
-        '''
-        ElectraForPreTraining: parameters in 'config' ; inherits from PreTrainedModel ; also a nn.Module subclass.
-        from_pretrained: a string, model id of a pretrained model hosted inside a model repo on huggingface.co
-        '''
+        self.tokenizer = ElectraTokenizerFast.from_pretrained("google/electra-base-discriminator") #identical to Bert tokenizer fake_sentence
+        self.discriminator = ElectraForPreTraining.from_pretrained("google/electra-base-discriminator")
+        #ElectraForPreTraining: parameters in 'config' ; inherits from PreTrainedModel ; also a nn.Module subclass.
+        #from_pretrained: a string, model id of a pretrained model hosted inside a model repo on huggingface.co
 
     def forward(self, fake_sentence):
-        #with profiler.record_function('forward pass'):
         fake_tokens = tokenizer.tokenize(fake_sentence) #break apart each words.
         fake_inputs = tokenizer.encode(fake_tokens, return_tensors="pt") #start, end token added. #padding이 따로 안보이는데? #return as pytorch tensor
         discriminator_outputs = self.discriminator(fake_inputs)
         predictions = torch.round((torch.sign(discriminator_outputs[0]) + 1) / 2)
         return predictions
 
-test_model = electra()
-#with profiler.profile(with_stack=True, profile_memory=True) as prof:
-predictions = test_model.forward(fake_inputs)
-[print("%7s" % token, end="") for token in fake_tokens]
-[print("%7s" % int(prediction), end="") for prediction in predictions.squeeze().tolist()]
+#with profile(activities=[ProfilerActivity.CPU],profile_memory=False,with_stack=True,record_shapes=True) as prof: #for CLI
+with profile(on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/electra_base'),profile_memory=True,with_stack=True,record_shapes=True) as prof: #for tensorboard
+    with record_function("model_inference"):
+        fake_sentence = "The quick brown fox fake over lazy dog" *  16 #길이 128로 맞추기. (need to check if this is all inputted.) + if padded.
+        test_model = electra()
+        predictions = test_model(fake_sentence)
+        [print("%7s" % int(prediction), end="") for prediction in predictions.squeeze().tolist()]
 
-#print(prof.key_averages(group_by_stack_n=5).table(sort_by='self_cpu_time_total'),row_limit=5)
-
-#writer.add_graph(test_model, fake_inputs)
-#writer.close()
-#t.toc()
+#end of profile scope
+#print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10)) #print cpu profile result
+print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=10)) #print profile result
+#print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10)) #print profile result
+#prof.export_chrome_trace("trace.json") #tracing fuctionality
+#print(prof.key_averages(group_by_stack_n=5).table(sort_by="self_cpu_time_total", row_limit=2))
